fb09ba024f05dee99fedef771a0cfe18012104cb
F4T-ES0035 <f4t-es0035@F4T-ES0035s-MacBook-Air.local>
2022-03-15 00:30:54 +0100

    added dependencies and created structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
