ce6adf271e1befd763716fcefd3c59dfa637f8d7
F4T-ES0035 <f4t-es0035@F4T-ES0035s-MacBook-Air.local>
2022-03-16 00:41:30 +0100

    added some changes

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Previem",
+  headline: "Unlimited transiction from zero",
+  description: "Get access to exclusive app for publish anything for enojy.",
+  buttonLabel: "Send email",
+  imgStart: false,
+  img: require("../../assets/svg1.svg"),
+  alt: "Card",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,22 +1,67 @@
 import React from "react";
-import { Button } from "react-scroll";
+import { Button } from "../ButtonElements";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Colum1,
+  Colum2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from "./InfoElements";
 
-const InfoSection = () => {
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  primary,
+  dark,
+  dark2,
+}) => {
   return (
     <>
-      <InfoContainer id={id}>
+      <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <Colum1>
               <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to="home">button</Button>
+                  <Button
+                    to="home"
+                    smooth={true}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >
+                    {buttonLabel}
+                  </Button>
                 </BtnWrap>
               </TextWrapper>
             </Colum1>
+            <Colum2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Colum2>
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
